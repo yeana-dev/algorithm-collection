6da0466b69e2d85b4fb11ee1b0b1c207f6845863
yeana0324 <yeana.dev@gmail.com>
2021-12-12 20:51:43 -0600

    Added Singly Linked List

---
+++ data_structure/singlyLinkedList/singlyLinkedList.js
@@ -0,0 +1,30 @@
+const Node = require("./Node");
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    if (tail === null) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode()) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+}
+
+module.exports = LinkedList;
